0b288dc1e089619a97f7583699ded4fff08fe35d
Konstantin Andreev <kandreev.cg@gmail.com>
2025-10-07 23:58:33 +0300

    создано окно и виджеты

--- Cataas.py
+++ Cataas.py
@@ -1,1 +1,20 @@
 from tkinter import *
+from PIL import Image, ImageTk
+import requests
+from io import BytesIO
+
+window = Tk()
+window.title("Cats!")
+window.geometry("600x480")
+
+label1 = Label()
+label.pack()
+
+URL = "https://cataas.com/cat"
+img = load.image(URL)
+
+if img:
+    label.config(image=img)
+    label1.image = img
+
+window.mainloop()
